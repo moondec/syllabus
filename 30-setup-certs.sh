188a5410799012b413abd28608e30b5591665da1
Marek Urbaniak <murbaniak76@gmail.com>
2026-06-08 16:57:25 +0200

    fix: make DISABLE_SSL parsing more robust and remove ssl config from HTTP mode

--- 30-setup-certs.sh
+++ 30-setup-certs.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
+# Normalize variables (lowercase, remove spaces and quotes)
+VAL_DISABLE_SSL=$(echo "$DISABLE_SSL" | tr '[:upper:]' '[:lower:]' | tr -d ' "\''')
+VAL_HTTP_ONLY=$(echo "$HTTP_ONLY" | tr '[:upper:]' '[:lower:]' | tr -d ' "\''')
+
 # Determine if we should use HTTP only
-if [ "$DISABLE_SSL" = "true" ] || [ "$HTTP_ONLY" = "true" ]; then
+if [ "$VAL_DISABLE_SSL" = "true" ] || [ "$VAL_DISABLE_SSL" = "1" ] || [ "$VAL_HTTP_ONLY" = "true" ] || [ "$VAL_HTTP_ONLY" = "1" ]; then
     echo "DISABLE_SSL or HTTP_ONLY is enabled. Using HTTP-only Nginx configuration."
     cp /etc/nginx/nginx-http.conf /etc/nginx/conf.d/default.conf
+    # Remove the SSL config template to ensure it's not accidentally loaded
+    rm -f /etc/nginx/nginx-ssl.conf
 else
     echo "SSL is enabled. Checking certificates..."
     mkdir -p /etc/nginx/ssl
